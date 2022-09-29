d3f206db6524c40908b6024ce7a012c8341d7c39
mr7aali <mr7aali@gmail.com>
2022-09-29 12:55:51 +0600

    added json

--- src/components/CartContainer/CartContaier.js
+++ src/components/CartContainer/CartContaier.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cart from './Cart/Cart';
 import './CartCOntainer.css';
 
 const CartContaier = () => {
+    const [cart,setCart]=useState([]);
+    useEffect(()=>{
+        fetch('fakedb.json')
+        .then(res=>res.json)
+        .then(data=>console.log(data))
+    },[])
+
     return (
         <div className='full-container'>
             <h1> Mainhealthtain-Your-Health </h1>
