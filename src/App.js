a4a2133b17b85f8f28be8ea65a7d572b0739b7c5
mr7aali <mr7aali@gmail.com>
2022-09-29 22:54:05 +0600

    added fav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import logo from './logo.svg';
+ import logo from './logo.svg';
 import './App.css';
 import Blog from './component/Blog/Blog';
 import Container from './component/Container/Container';
